bf962e03a15a490e06c0d6db6a46af7e697be91c
Dominik Walser <dominikwalser@Dominiks-MacBook-Pro.local>
2018-09-13 21:13:22 +0200

    modification for prod and dev

--- backend/settings.py
+++ backend/settings.py
@@ -54,21 +54,38 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'backend.urls'
 
-TEMPLATES = [
-  {
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': ['static'],
-    'APP_DIRS': True,
-    'OPTIONS': {
-      'context_processors': [
-        'django.template.context_processors.debug',
-        'django.template.context_processors.request',
-        'django.contrib.auth.context_processors.auth',
-        'django.contrib.messages.context_processors.messages',
-      ],
+if 'ON_HEROKU' in os.environ:
+  TEMPLATES = [
+    {
+      'BACKEND': 'django.template.backends.django.DjangoTemplates',
+      'DIRS': ['static'],
+      'APP_DIRS': True,
+      'OPTIONS': {
+        'context_processors': [
+          'django.template.context_processors.debug',
+          'django.template.context_processors.request',
+          'django.contrib.auth.context_processors.auth',
+          'django.contrib.messages.context_processors.messages',
+        ],
+      },
     },
-  },
-]
+  ]
+else:
+  TEMPLATES = [
+    {
+      'BACKEND': 'django.template.backends.django.DjangoTemplates',
+      'DIRS': ['static/dev'],
+      'APP_DIRS': True,
+      'OPTIONS': {
+        'context_processors': [
+          'django.template.context_processors.debug',
+          'django.template.context_processors.request',
+          'django.contrib.auth.context_processors.auth',
+          'django.contrib.messages.context_processors.messages',
+        ],
+      },
+    },
+  ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
@@ -106,24 +123,6 @@ else:
       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
   }
-
-# Password validation
-# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
-#
-# AUTH_PASSWORD_VALIDATORS = [
-#   {
-#     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#   },
-#   {
-#     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#   },
-#   {
-#     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#   },
-#   {
-#     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#   },
-# ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
