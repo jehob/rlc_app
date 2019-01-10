6d83d208c0cad2bd2684329e4576a948b1f90264
Dominik Walser <jehob@web.de>
2019-01-10 14:02:30 +0100

    copyright changed to 2019

--- backend/recordmanagement/views/client.py
+++ backend/recordmanagement/views/client.py
@@ -1,5 +1,5 @@
 #  rlcapp - record and organization management software for refugee law clinics
-#  Copyright (C) 2018  Dominik Walser
+#  Copyright (C) 2019  Dominik Walser
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Affero General Public License as
